2e389f5d46794b588796376abf28df0b20a36041
Victor Barão Nunes <victorbaraonunes@gmail.com>
2020-01-19 13:07:34 -0300

    add exec + add feature to get files from commit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
 
-const program = require('commander'),
-    package = require('./package.json');
+const program = require('commander');
+const package = require('./package.json');
+const exec = require('child_process').exec;
 
 program.version(package);
 program.description('some git utilities');
@@ -10,7 +11,30 @@ program.option('-c, --commit <commit>', 'commit hash to run. min length = 7');
 program.parse(process.argv);
 
 if (program.commit) {
-    if (program.commit.length < 7) return console.log('commit hash too short :/');
+    if (program.commit.length < 7) {
+        console.error('commit hash too short :/');
+        process.exit(1);
+    }
+
+    exec(`git show --pretty="" --name-only ${program.commit}`, (error, stdout, stderr) => {
+        console.log('error', error);
+
+        if (error) {
+            console.error('execution failed :/', error);
+            process.exit(1);
+        }
+
+        console.log('stdout', stdout.split('\n'));
+        console.log('stderr', stderr);
+
+        stdout.split('\n').forEach((file) => {
+            if (!file) return;
+
+            exec(`echo >> `, () => {
+
+            });
+        });
+    });
 } else {
     program.help();   
 }
